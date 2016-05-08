c1d9e4c9dd350e7ab9e3ca7c91467c086f8a0f0e
Glenn Rempe <glenn@rempe.us>
2016-05-07 21:52:21 -0700

    Split monolithic file into SRP module, Client class, Verifier class files.

---
+++ lib/srp/client.rb
@@ -0,0 +1,56 @@
+module SRP
+  class Client
+    attr_reader :N, :g, :k, :a, :A, :S, :K, :M, :H_AMK
+
+    def initialize group=1024
+      # select modulus (N) and generator (g)
+      @N, @g = SRP.Ng group
+      @k = SRP.calc_k(@N, @g)
+    end
+
+    def start_authentication
+      generate_A
+    end
+
+    # Process initiated authentication challenge.
+    # Returns M if authentication is successful, false otherwise.
+    # Salt and B should be given in hex.
+    def process_challenge username, password, xsalt, xbb
+      bb = xbb.to_i(16)
+      # SRP-6a safety check
+      return false if (bb % @N) == 0
+
+      x = SRP.calc_x(username, password, xsalt)
+      u = SRP.calc_u(@A, xbb, @N)
+
+      # SRP-6a safety check
+      return false if u == 0
+
+      # calculate session key
+      @S = "%x" % SRP.calc_client_S(bb, @a, @k, x, u, @N, @g)
+      @K = SRP.sha1_hex(@S)
+
+      # calculate match
+      @M = "%x" % SRP.calc_M(username, xsalt, @A, xbb, @K, @N, @g)
+
+      # calculate verifier
+      @H_AMK = "%x" % SRP.calc_H_AMK(@A, @M, @K, @N, @g)
+
+      return @M
+    end
+
+    def verify server_HAMK
+      return false unless @H_AMK
+      @H_AMK == server_HAMK
+    end
+
+    def random_bignum
+      SRP.bigrand(32).hex
+    end
+
+    def generate_A
+      @a ||= random_bignum
+      @A = "%x" % SRP.calc_A(@a, @N, @g)
+    end
+  end # Client
+end
---
+++ lib/srp/verifier.rb
@@ -0,0 +1,81 @@
+module SRP
+  class Verifier
+    attr_reader :N, :g, :k, :A, :B, :b, :S, :K, :M, :H_AMK
+
+    def initialize group=1024
+      # select modulus (N) and generator (g)
+      @N, @g = SRP.Ng group
+      @k = SRP.calc_k(@N, @g)
+    end
+
+    # Initial user creation for the persistance layer.
+    # Not part of the authentication process.
+    # Returns { <username>, <password verifier>, <salt> }
+    def generate_userauth(username, password)
+      @salt ||= random_salt
+      x = SRP.calc_x(username, password, @salt)
+      v = SRP.calc_v(x, @N, @g)
+      return {:username => username, :verifier => "%x" % v, :salt => @salt}
+    end
+
+    # Authentication phase 1 - create challenge.
+    # Returns Hash with challenge for client and proof to be stored on server.
+    # Parameters should be given in hex.
+    def get_challenge_and_proof username, xverifier, xsalt, xaa
+      # SRP-6a safety check
+      return false if (xaa.to_i(16) % @N) == 0
+      generate_B(xverifier)
+      return {
+        :challenge    => {:B => @B, :salt => xsalt},
+        :proof        => {:A => xaa, :B => @B, :b => "%x" % @b,
+                          :I => username, :s => xsalt, :v => xverifier}
+      }
+    end
+
+    # returns H_AMK on success, None on failure
+    # User -> Host:  M = H(H(N) xor H(g), H(I), s, A, B, K)
+    # Host -> User:  H(A, M, K)
+    def verify_session proof, client_M
+      @A = proof[:A]
+      @B = proof[:B]
+      @b = proof[:b].to_i(16)
+      username = proof[:I]
+      xsalt = proof[:s]
+      v = proof[:v].to_i(16)
+
+      u = SRP.calc_u(@A, @B, @N)
+      # SRP-6a safety check
+      return false if u == 0
+
+      # calculate session key
+      @S = "%x" % SRP.calc_server_S(@A.to_i(16), @b, v, u, @N)
+      @K = SRP.sha1_hex(@S)
+
+      # calculate match
+      @M = "%x" % SRP.calc_M(username, xsalt, @A, @B, @K, @N, @g)
+
+      if @M == client_M
+        # authentication succeeded
+        @H_AMK = "%x" % SRP.calc_H_AMK(@A, @M, @K, @N, @g)
+        return @H_AMK
+      end
+      return false
+    end
+
+    def random_salt
+      "%x" % SRP.bigrand(10).hex
+    end
+
+    def random_bignum
+      SRP.bigrand(32).hex
+    end
+
+    # generates challenge
+    # input verifier in hex
+    def generate_B xverifier
+      v = xverifier.to_i(16)
+      @b ||= random_bignum
+      @B = "%x" % SRP.calc_B(@b, k, v, @N, @g)
+    end
+  end
+end
