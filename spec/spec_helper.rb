40dfdf9b6258d5b18fe764f4b6197ee5e3ed927c
Glenn Rempe <glenn@rempe.us>
2016-05-12 11:41:16 -0700

    Add coco and coveralls code coverage config.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,15 @@
 # encoding: utf-8
+
+# coveralls.io and coco are incompatible. Run each in their own env.
+if ENV['TRAVIS'] || ENV['CI'] || ENV['JENKINS_URL'] || ENV['TDDIUM'] || ENV['COVERALLS_RUN_LOCALLY']
+  # coveralls.io : web based code coverage
+  require 'coveralls'
+  Coveralls.wear!
+else
+  # coco : local code coverage
+  require 'coco'
+end
+
 require 'srp'
 
 # Allow use of the old deprecated rspec syntax
--- srp.gemspec
+++ srp.gemspec
@@ -36,5 +36,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rake', '~> 11.0'
   spec.add_development_dependency 'rspec', '~> 3.4'
   spec.add_development_dependency 'pry', '~> 0.10'
+  spec.add_development_dependency 'coveralls', '~> 0.8'
+  spec.add_development_dependency 'coco', '~> 0.14'
   spec.add_development_dependency 'wwtd', '~> 1.3'
 end
