1afc325a055b3ddd475475a84ae4914d0143abb2
Glenn Rempe <glenn@rempe.us>
2016-05-16 10:56:43 -0700

    Enhance the Ruby->JSRP client/server demo

--- examples/clients/javascript/app.js
+++ examples/clients/javascript/app.js
@@ -2,55 +2,63 @@ var client = new jsrp.client()
 var serverAddr = 'http://localhost:4567/authenticate'
 var username = 'leonardo'
 var password = 'capricciosa'
+var length = 4096
 
 $(document).ready(function () {
   'use strict'
 
-  console.log('Starting Authentication')
+  $('#statusP0').append('P1 : START\n')
+  $('#statusP0').append('P1 : username : ' + username + '\n')
+  $('#statusP0').append('P1 : password : ' + password + '\n')
+  $('#statusP0').append('P1 : length : ' + length + ' bits\n')
 
   $(document).ajaxError(function (event, request, settings) {
-    console.log(event)
-    console.log(request)
-    console.log(settings)
+    console.error(event)
+    console.error(request)
+    console.error(settings)
   })
 
-  client.init({ username: username, password: password, length: 4096 }, function () {
+  client.init({ username: username, password: password, length: length }, function () {
     client.createVerifier(function (err, result) {
-      // result will contain the necessary values the server needs to
-      // authenticate this user in the future.
-      // sendSaltToServer(result.salt)
-      // sendVerifierToServer(result.verifier)
-
       if (err) {
-         console.log(err.stack);
-       }
+        console.error(err.stack)
+      }
 
+      $('#statusP0').append('P0 : generated client salt : ' + result.salt + '\n')
+      $('#statusP0').append('P0 : generated client verifier : ' + result.verifier + '\n')
+      $('#statusP0').append('P0 : (SIMULATED) registering user with username, salt, and verifier.\n')
+
+      // Phase 1
+      // Send : username and A
+      // Receive : salt and B
+      // Calculate : M
+      //
       var A = client.getPublicKey()
-
-      // Auth Phase 1
-      console.log('P1 : Sending username and A to server')
+      $('#statusP1').append('P1 : client A : ' + A + '\n')
+      $('#statusP1').append('P1 : Sending username and A to server\n')
       $.post(serverAddr, { username: username, A: A }, function (data) {
-        console.log('P1 : Received salt : ', data.salt)
-        console.log('P1 : Received B : ', data.B)
+        $('#statusP1').append('P1 : Received salt : ' + data.salt + '\n')
+        $('#statusP1').append('P1 : Received B : ' + data.B + '\n')
         client.setSalt(data.salt)
         client.setServerPublicKey(data.B)
 
         var clientM = client.getProof()
-        console.log('P1 : calculated client M : ', clientM)
+        $('#statusP1').append('P1 : calculated client M : ' + clientM + '\n')
 
-        // Auth Phase 2
-        console.log('P2 : Sending username and M to server')
+        // Phase 2
+        // Send : username and M
+        // Receive : H_AMK
+        // Confirm client and server H_AMK values match, use shared key K
+        //
+        $('#statusP2').append('P2 : Sending username and client M to server\n')
         $.post(serverAddr, { username: username, M: clientM }, function (data) {
-          console.log('P2 : Received server H_AMK : ', data.H_AMK)
+          $('#statusP2').append('P2 : Received server H_AMK : ' + data.H_AMK + '\n')
           if (client.checkServerProof(data.H_AMK)) {
-            console.log('P2 : Client and server H_AMK match. Authenticated!')
-            console.log('P2 : Client and server negotiated shared key K : ', client.getSharedKey())
-            $('#status').html('Authenticated!')
-            $('#username').html(username)
-            $('#K').html(client.getSharedKey())
+            $('#statusP2').append('P2 : H_AMK values match!\n')
+            $('#statusP2').append('P2 : Shared Secret K : ' + client.getSharedKey() + '\n')
+            $('#statusP2').append('\nAUTHENTICATION COMPLETE!')
           } else {
-            $('#status').html('Authenticated FAILED')
-            console.log('P2 : Client and server H_AMK DO NOT match. Authentication failed!')
+            $('#statusErr').append('P2 : Error : Auth Failed : Client and server H_AMK did not match.')
           }
         }, 'json')
       }, 'json')
