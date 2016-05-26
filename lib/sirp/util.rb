2a060a07fb2a4498b30d16146f83acda7686d701
Glenn Rempe <glenn@rempe.us>
2016-05-26 10:24:55 -0700

    Split common utility functions out to their own file

---
+++ lib/sirp/util.rb
@@ -0,0 +1,73 @@
+module SIRP
+  # Convert a hex string to an a array of Integer bytes by first converting
+  # the String to hex, and then converting that hex to an Array of Integer bytes.
+  #
+  # @param str [String] a string to convert
+  # @return [Array<Integer>] an Array of Integer bytes
+  def hex_to_bytes(str)
+    [str].pack('H*').unpack('C*')
+  end
+
+  # Convert a number to a downcased hex string, prepending '0' to the
+  # hex string if the hex conversion resulted in an odd length string.
+  #
+  # @param num [Integer] a number to convert to a hex string
+  # @return [String] a hex string
+  def num_to_hex(num)
+    hex_str = num.to_s(16)
+    even_hex_str = hex_str.length.odd? ? '0' + hex_str : hex_str
+    even_hex_str.downcase
+  end
+
+  # Applies a one-way hash function, either SHA1 or SHA256, on an
+  # unpacked hex string. It will generate the same
+  # one-way hash value for a string that has been unpacked as if the
+  # hash function had been applied to the string directly.
+  #
+  #    'foo'.unpack('H*')
+  #    => ["666f6f"]
+  #
+  #    > sha_hex('foo'.unpack('H*')[0], Digest::SHA256)
+  #    => "2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae"
+  #    > Digest::SHA256.hexdigest 'foo'
+  #    => "2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae"
+  #
+  # @param h [String] a hex string to hash
+  # @param hash_klass [Digest::SHA1, Digest::SHA256] The hash class that responds to hexdigest
+  # @return [String] a hex string representing the result of the one way hash function
+  def sha_hex(h, hash_klass)
+    hash_klass.hexdigest([h].pack('H*'))
+  end
+
+  # Applies a one-way hash function, either SHA1 or SHA256, on the string provided.
+  #
+  # @param s [String] a string to hash
+  # @param hash_klass [Digest::SHA1, Digest::SHA256] The hash class that responds to hexdigest
+  # @return [String] a hex string representing the result of the one way hash function
+  def sha_str(s, hash_klass)
+    hash_klass.hexdigest(s)
+  end
+
+  # Constant time string comparison.
+  # Extracted from Rack::Utils
+  # https://github.com/rack/rack/blob/master/lib/rack/utils.rb
+  #
+  # NOTE: the values compared should be of fixed length, such as strings
+  # that have already been processed by HMAC. This should not be used
+  # on variable length plaintext strings because it could leak length info
+  # via timing attacks. The user provided value should always be passed
+  # in as the second parameter so as not to leak info about the secret.
+  #
+  # @param a [String] the private value
+  # @param b [String] the user provided value
+  # @return [true, false] whether the strings match or not
+  def secure_compare(a, b)
+    return false unless a.bytesize == b.bytesize
+
+    l = a.unpack('C*')
+
+    r, i = 0, -1
+    b.each_byte { |v| r |= v ^ l[i+=1] }
+    r == 0
+  end
+end
--- spec/sirp_spec.rb
+++ spec/sirp_spec.rb
@@ -6,6 +6,7 @@ require 'spec_helper'
 #
 describe SIRP do
   include SIRP
+
   before :all do
     @N = '115b8b692e0e045692cf280b436735c77a5a9e8a9e7ed56c965f87db5b2a2ece3'.to_i(16)
     @g = 2
@@ -14,50 +15,6 @@ describe SIRP do
     @salt = '01ebb2496e4e8d32e6f7967ee9fec64e'
     @a = '7ec87196e320a2f8dfe8979b1992e0d34439d24471b62c40564bb4302866e1c2'.to_i(16)
     @b = '8143e2f299852a05717427ea9d87c6146e747d0da6e95f4390264e55a43ae96'.to_i(16)
-  end
-
-  context 'hex_to_bytes' do
-    it 'should calculate expected results' do
-      expect(hex_to_bytes('abcdef0123456789'))
-        .to eq [171, 205, 239, 1, 35, 69, 103, 137]
-    end
-  end
-
-  context 'num_to_hex' do
-    it 'should calculate expected results' do
-      num = 999_999_999_999
-      expect(num_to_hex(num))
-        .to eq 'e8d4a50fff'
-      expect('e8d4a50fff'.hex).to eq num
-    end
-  end
-
-  context 'sha_hex' do
-    it 'should calculate expected results for SHA1' do
-      str = 'foo'
-      str_unpacked = str.unpack('H*')[0]
-      str_sha = Digest::SHA1.hexdigest(str)
-      expect(sha_hex(str_unpacked, Digest::SHA1)).to eq str_sha
-    end
-
-    it 'should calculate expected results for SHA256' do
-      str = 'foo'
-      str_unpacked = str.unpack('H*')[0]
-      str_sha = Digest::SHA256.hexdigest(str)
-      expect(sha_hex(str_unpacked, Digest::SHA256)).to eq str_sha
-    end
-  end
-
-  context 'sha_str' do
-    it 'should calculate expected results for SHA1' do
-      expect(sha_str('foo', Digest::SHA1))
-        .to eq '0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33'
-    end
-
-    it 'should calculate expected results for SHA256' do
-      expect(sha_str('foo', Digest::SHA256))
-        .to eq '2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae'
-    end
   end
 
   context 'mod_exp' do
